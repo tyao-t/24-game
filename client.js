965e2799e4fcf41919ddbb6933e3c2199b748c1f
Your Name <you@example.com>
2021-03-11 15:19:11 -0500

    revert back to card pngs

--- client.js
+++ client.js
@@ -106,7 +106,8 @@ socket.on('count-down', count => {
 
 socket.on('fourCards', four => {
     for (let i=1;i<=4;i++) {
-        document.getElementById('c'+i).innerHTML = (four[i-1].substring(0,1));
+        console.log(four[i-1]);
+        document.getElementById('card'+i).src = './cards/PNG' + four[i-1];
     }
 })
 
